50267ef29a42222b3d72124687117b496aac0af7
mariyatarasenko88 <tmv1231@yandex.ru>
2023-10-27 20:07:41 +0300

    Написала UserController

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,114 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.model.dto.NewPasswordDTO;
+import ru.skypro.homework.model.dto.User;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+
+    @Operation(
+            tags = "Пользователи",
+            summary = "Получить информацию об авторизованном пользователе",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content()
+                    )
+            }
+    )
+    @GetMapping("/me")
+    public User getUser(@RequestBody User user){
+        return new User();
+    }
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновление пароля",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content()
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content()
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content()
+                    )
+            }
+    )
+    @PostMapping("/set_password")
+    public NewPasswordDTO setUserPassword(@RequestBody NewPasswordDTO newPasswordDTO) {
+        return new NewPasswordDTO();
+    }
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновить информацию об авторизованном пользователе",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content()
+                    )
+            }
+    )
+    @PatchMapping("/me")
+    public User updateUser(@RequestBody User user) {
+        System.out.println("updated");
+        return new User();
+    }
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновить аватар авторизованного пользователя",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content()
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content()
+                    )
+            }
+    )
+    @PatchMapping("/me/image")
+    public User updateUserImage(@RequestBody User user) {
+        System.out.println("image updated");
+        return new User();
+    }
+}
